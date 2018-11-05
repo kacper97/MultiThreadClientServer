01ac1151b6de86dda1ff79ac18676c1da94fb842
unknown <kacperwoloszyn4@gmail.com>
2018-11-05 11:38:55 +0000

    Basic planning of assignment

---
+++ src/ClientA2.java
@@ -0,0 +1,11 @@
+
+public class ClientA2 {
+
+	/*
+	 * Enters Student Id + submits request
+	 * 
+	 * Validate Communication by
+		displaying HostName and IPAddress
+		in the Client/Server windows with all messages sent
+	 */
+}
